f7604795b99f9fd661367ea54f928c2dbd652499
DungNguyenCoder <dungnickchinh1909@gmail.com>
2025-01-18 18:59:28 +0700

    Day 1

---
+++ Array_String/11.ContainerWithMostWater.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//https://leetcode.com/problems/container-with-most-water/description/?envType=problem-list-v2&envId=array
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int l = 0;
+        int r = n - 1;
+        int maxArea = 0;
+        int water;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                water = height[l] * (r - l);
+                ++l;
+            } else {
+                water = height[r] * (r - l);
+                --r;
+            }
+            maxArea = max(maxArea, water);
+        }
+        return maxArea;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> height(n);
+    for(int i = 0; i < n; i++) {
+        cin >> height[i];
+    }
+    Solution a;
+    cout << a.maxArea(height);
+}
---
+++ Array_String/15. 3Sum.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//https://leetcode.com/problems/3sum/?envType=problem-list-v2&envId=array
+
+int binarySearch(vector<int> nums, int l, int r, int x) {
+    while(l <= r) {
+        int m = (l+r)/2;
+        if(nums[m] == x) {
+            return m;
+        }
+        else if(nums[m] > x) {
+            r = m-1;
+        }
+        else {
+            l = m+1;
+        }
+    }
+    return -1;
+}
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+
+                    l++;
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+int main() {
+    int n; cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++)
+        cin >> nums[i];
+    Solution a;
+    vector<vector<int>> v = a.threeSum(nums);
+    for(int i = 0; i < v.size(); i++) {
+        for(int j = 0; j < v[i].size(); j++) {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
